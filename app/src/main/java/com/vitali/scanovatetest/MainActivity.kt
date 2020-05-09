b6212ab7b32958ac4f1743293122199b53503902
VitaliPekelis <vitali@blazepod.co>
2020-05-09 19:48:00 +0300

    add version tv

--- app/src/main/java/com/vitali/scanovatetest/MainActivity.kt
+++ app/src/main/java/com/vitali/scanovatetest/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvVersion.text = "Version Code: ${BuildConfig.VERSION_CODE}\nVersion Name: ${BuildConfig.VERSION_NAME}\nFlavor: ${BuildConfig.FLAVOR}"
     }
 
     fun openCamera(v:View) {
